485c9acd280195d95fd6c91e5a19c103341f0fef
Andr�s Felipe Verdugo <andres.verdugo.quevedo@gmail.com>
2017-12-08 21:06:52 -0500

    todo listo!

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,6 +4,18 @@ import { Meteor } from 'meteor/meteor';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId },
+      ],
+    });
+  });
+}
+
 Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
@@ -30,5 +42,18 @@ Meteor.methods({
     check(setChecked, Boolean);
  
     Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+  'tasks.setPrivate'(taskId, setToPrivate) {
+    check(taskId, String);
+    check(setToPrivate, Boolean);
+ 
+    const task = Tasks.findOne(taskId);
+ 
+    // Make sure only the task owner can make a task private
+    if (task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+ 
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
   },
 });
